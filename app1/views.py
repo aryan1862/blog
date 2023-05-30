09448dcca7dbf25d10b3d6c61e40ff6983b696ee
Aryan Kumar Singh <aryankumarsingh1503@gmail.com>
2023-05-30 22:58:59 +0530

    working on it

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def starting_page(request):
+    return render(request,"app1/index.html")
+
+def posts(request):
+    pass
+
+def post_detail(request):
+    pass
